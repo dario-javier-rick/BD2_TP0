49360913cb2fc0e65f91d32eb90c7b0c62253ad6
Dario Javier Rick <dario_rick@outlook.com>
2018-08-27 20:45:51 -0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -8,32 +8,38 @@ CREATE OR REPLACE FUNCTION get_pop_variation_rate(_idPais INTEGER)
   RETURNS real AS
 $BODY$
 DECLARE
-
-	crecimiento REAL;
-	
+	PoblacionCenso1 INTEGER;
+	FechaCenso1 DATE;
+	PoblacionCenso2 INTEGER;
+	FechaCenso2 DATE;
+	Crecimiento REAL;
+		
 BEGIN
 
 	RAISE NOTICE 'Se invoca get_pop_variation_rate()';
   
-  SELECT 
-    (SELECT Poblacion
-    FROM Censos
-    JOIN Paises
-      ON Censos.IdPais = Paises.Id
-    WHERE Paises.Id = _idPais
-    ORDER BY Censos.Id DESC
-    LIMIT 1 OFFSET 1)
-    * 100 /
-    (SELECT Poblacion
-    FROM Censos
-    JOIN Paises
-      ON Censos.IdPais = Paises.Id
-    WHERE Paises.Id = _idPais
-    ORDER BY Censos.Id DESC
-    LIMIT 1 OFFSET 2)
-  INTO crecimiento;
-  
-	RETURN crecimiento;
+	  SELECT Poblacion, FechaCenso 
+	    FROM Censos
+	    INTO PoblacionCenso1, FechaCenso1
+	    JOIN Paises
+	      ON Censos.IdPais = Paises.Id
+	    WHERE Paises.Id = _idPais
+	    ORDER BY FechaCenso DESC
+	    LIMIT 1 OFFSET 1;
+
+	  SELECT Poblacion, FechaCenso
+	    FROM Censos
+	    INTO PoblacionCenso2, FechaCenso2
+	    JOIN Paises
+	      ON Censos.IdPais = Paises.Id
+	    WHERE Paises.Id = _idPais
+	    ORDER BY FechaCenso DESC
+	    LIMIT 1 OFFSET 2
+
+	  SELECT (PoblacionCenso1 * 100 / PoblacionCenso2) / DATEDIFF(year, FechaCenso1, FechaCenso2)
+	  INTO Crecimiento;
+
+	RETURN Crecimiento;
 
 	EXCEPTION
 	WHEN OTHERS THEN
