dc9982ec61cf75a3e0cc6a06cfe8291355bd6d8c
Dario Javier Rick <dario_rick@outlook.com>
2018-08-27 18:34:57 -0300

    Update 1.sql
    Tablas completas

--- 1.sql
+++ 1.sql
@@ -3,20 +3,37 @@
 ----------------------------------------------------------------------------------------
 -- Tablas del trabajo práctico N°0
 
+/*
+DROP DATABASE IF EXISTS TP0;
+DROP TABLE IF EXISTS Censos;
+DROP TABLE IF EXISTS Paises;
+DROP TABLE IF EXISTS Continentes;
+*/
+
 CREATE DATABASE TP0;
 
-CREATE TABLE Continente
+CREATE TABLE Continentes
 (
-Id SERIAL,
-Nombre VARCHAR(40) NOT NULL
+  Id SERIAL,
+  Nombre VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE Paises
 (
-Id SERIAL,
-Nombre VARCHAR(40) NOT NULL,
-FechaIndependencia DATE NOT NULL,
-IdContinente INTEGER NOT NULL,
-FormaGobierno VARCHAR(40),
-Poblacion BIGINT
+  Id SERIAL,
+  Nombre VARCHAR(40) NOT NULL,
+  IdContinente INTEGER NOT NULL,
+  FormaGobierno VARCHAR(40) NOT NULL,
+  Poblacion BIGINT NOT NULL,
+  FechaIndependencia DATE NOT NULL,
+  FOREIGN KEY (IdContinente) REFERENCES Continentes (Id)
+);
+
+CREATE TABLE Censos
+(
+  Id SERIAL,
+  FechaCenso DATE NOT NULL,
+  IdPais INTEGER NOT NULL,
+  Poblacion BIGINT NOT NULL,
+  FOREIGN KEY (IdPais) REFERENCES Paises (Id)
 );
