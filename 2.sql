81c3b513cda37509ec80028f323564d4079c3ce9
Dario Javier Rick <dario_rick@outlook.com>
2018-08-27 22:45:49 -0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -2,4 +2,7 @@
 
 ----------------------------------------------------------------------------------------
 
-COPY Paises(Id,Nombre,IdContinente,FormaGobierno,Poblacion,FechaIndependencia) FROM 'paises.csv' DELIMITER ',' CSV HEADER;
+COPY Continentes(Id,Nombre) FROM 'C:\Datos_paises - Continente.csv' DELIMITER ',' CSV HEADER;
+COPY Paises(Id,Nombre,FechaIndependencia,IdContinente,FormaGobierno,Poblacion) FROM 'C:\Datos_paises - Pais.csv' DELIMITER ',' CSV HEADER;
+COPY Censos(IdPais,FechaCenso,Poblacion) FROM 'C:\Datos_paises - Censo.csv' DELIMITER ',' CSV HEADER;
+COPY Limites(Pais_Id1,Pais_Id2,ExtensionFrontera) FROM 'C:\Datos_paises - Frontera.csv' DELIMITER ',' CSV HEADER;
